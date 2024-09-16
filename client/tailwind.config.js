baa4ff8a0228b424022f2d15b01fc8d05349be86
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-16 12:50:15 +0200

    feat(structure): configure CSS color pallet and change the default font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#7209B7",         // Main purple/violet
+                primaryDeep: "#560BAD",      // Deeper shade of primary
+                primaryDark: "#480CA8",      // Darkest shade of primary
+
+                secondary: "#3F37C9",        // Main blue
+                secondaryBright: "#4361EE",  // Brighter blue
+                secondaryLight: "#4895EF",   // Lightest shade of blue
+
+                accent: "#B5179E",           // Main pink accent
+                accentBright: "#F72585",     // Brighter pink accent
+            },
+        },
+        fontFamily: {
+            "sans": ["Roboto", "sans-serif"],
+        }
     },
     plugins: [],
 };
