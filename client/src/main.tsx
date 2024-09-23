06a32150042ed2eed0bf57801d9021c6c94369b5
Enis Nezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-23 17:22:18 +0200

    feature(replace-tailwind-with-scss): implement old tailwind styling to SCSS

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./Router.tsx";
-import "./index.css";
+import "./styles/global.scss";
 
 const root = document.getElementById("root");
 
