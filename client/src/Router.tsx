0d5127b144a7186d7628007faf83e982bc22e3f9
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-24 00:45:37 +0200

    feature(auth): create login page

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,9 +1,16 @@
 import {createBrowserRouter} from "react-router-dom";
 import {App} from "./App.tsx";
+import {LoginPage} from "./pages/login/login.page.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App/>,
+    children: [
+      {
+        path: "login",
+        element: <LoginPage/>
+      }
+    ]
   }
 ]);
