de1a5964cb582343ea54eeb5318236ea92c0b401
enisnezirevic <122569557+enisnezirevic@users.noreply.github.com>
2024-09-29 18:01:20 +0200

    feature(auth): refactor auth service code

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,4 +1,5 @@
 import {apiClient} from "./api.service.ts";
+import {handleBadRequestError} from "../utils/error_handler.ts";
 
 type Login = {
   username: string;
@@ -24,18 +25,8 @@ const AuthService = {
       );
       return response.data;
     } catch (e: any) {
-      if (e.response) {
-        switch (e.response.status) {
-          case 400:
-            throw new Error("Please ensure your credentials are correct and try again.");
-          case 401:
-            throw new Error("Your credentials are incorrect. Please check your username and password and try again.");
-          default:
-            throw new Error("We encountered an unexpected issue. Please try again later.");
-        }
-      } else {
-        throw new Error("We were unable to connect to the server. Please check your internet connection and try again.");
-      }
+      const errorMessage = handleBadRequestError(e, "Please ensure your credentials are correct and try again.");
+      throw new Error(errorMessage);
     }
   },
 
@@ -51,34 +42,8 @@ const AuthService = {
       });
       return response.data;
     } catch (e: any) {
-      if (e.response) {
-        if (e.response.status === 400) {
-          const errorData = e.response.data;
-          if (errorData) {
-            let firstErrorMessage = "";
-
-            Object.entries(errorData).forEach(([_, value]) => {
-              if (Array.isArray(value) && value.length > 0) {
-                firstErrorMessage = value[0];
-              }
-            });
-
-            throw new Error(firstErrorMessage);
-          } else {
-            throw new Error(
-              "An unknown error occurred during registration. Please try again later."
-            );
-          }
-        } else {
-          throw new Error(
-            "We were unable to connect to the server. Please check your internet connection and try again."
-          );
-        }
-      } else {
-        throw new Error(
-          "An unexpected error occurred. Please check your network and try again."
-        );
-      }
+      const errorMessage = handleBadRequestError(e, "An unknown error occurred during registration. Please try again later.");
+      throw new Error(errorMessage);
     }
   }
 };
